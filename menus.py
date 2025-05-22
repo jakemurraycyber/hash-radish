1fa733be98e9c03b0153763b6c1b716e515cef22
Jake Murray <jake.murray9010@gmail.com>
2025-05-22 13:42:42 -0500

    began work on menu system

---
+++ menus.py
@@ -0,0 +1,45 @@
+# Class definition for Main and sub-menus for Hash Radish Program
+
+class Menu:
+
+    def __init__(self, title: str, options: list = []) -> None:
+        self.title = title
+        self.options = options
+
+    def displayMenu(self) -> str:
+        # Prints Menu title with same-length line break
+        lineBreak = '-' * len(self.title)
+        print(f"\n{self.title}")
+        print(lineBreak)
+        for option in self.options:
+            print(option.title())
+
+    def addOption(self, option: str) -> None:
+        self.options.append(option)
+
+
+class SubMenu(Menu):
+    def __init__(self, parent: str, title: str, options: list = []) -> None:
+        super().__init__(title, options)
+        self.parent = parent
+
+    # return to previous/parent menu
+    def goBack(self):
+        return self.parent.displayMenu()
+
+
+mainOptions = [
+    '1. user input',
+    '2. file input',
+    '3. line input'
+    ]
+
+mainMenu = Menu('Main Menu', mainOptions)
+mainMenu.addOption(('"x" to exit'))
+currentMenu = mainMenu
+currentMenu.displayMenu()
+
+algOptions = ["1. SHA512"]
+algMenu = SubMenu(mainMenu, "Algorithm Menu", algOptions)
+currentMenu = algMenu
+currentMenu.displayMenu()
