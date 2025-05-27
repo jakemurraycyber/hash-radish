3b56c7832965309f3550706a145810c7cb5ccb7e
Jake M <jake.murray9010@gmail.com>
2025-05-27 00:49:15 -0500

    Completed basic menu logic

--- menus.py
+++ menus.py
@@ -9,32 +9,46 @@ class Menu:
     def displayMenu(self) -> None:
         # Prints Menu title with same-length line break
         lineBreak = '-' * len(self.title)
+        optionCounter = 1
         print(f"\n{self.title}")
         print(lineBreak)
         for option in self.options:
-            print(option.title())
+            print(f"{str(optionCounter)}. {option.title()}")
+            optionCounter += 1
+        print('"exit" to terminate program')
 
     def addOption(self, option: str) -> None:
         self.options.append(option)
 
+    def getOptionIndex(self, choice: str) -> int:
+        # This is the method that maps user input ("1") to one of the menu items
+        # if the input is invalid, it returns -1
+        index = int(choice) -1
+        if 0 <= index < len(self.options):
+            return index
+        else:
+            return -1
+
 
 class SubMenu(Menu):
-    def __init__(self, parent: object, title: str, options: list[str] = []) -> None:
+    def __init__(self, parent: 'Menu', title: str, options: list[str] = []) -> None:
         super().__init__(title, options)
         self.parent = parent
 
     # return to previous/parent menu
-    def goBack(self):
+    def goBack(self) -> 'Menu':
         return self.parent
+    
+__all__ = ['SubMenu', 'Menu']
 
-
+'''
+# TESTING
 mainOptions = [
     '1. user input',
     '2. file input',
     '3. line input'
     ]
 
-# TESTING
 mainMenu = Menu('Main Menu', mainOptions)
 mainMenu.addOption(('"x" to exit'))
 currentMenu = mainMenu
@@ -44,3 +58,4 @@ algOptions = ["1. SHA512"]
 algMenu = SubMenu(mainMenu, "Algorithm Menu", algOptions)
 currentMenu = algMenu
 currentMenu.displayMenu()
+'''
